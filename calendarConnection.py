422df94759f44ce996391ee0eab4dcd3d347c494
Tilman Adler <tilman-git@t-animal.de>
2017-01-18 17:10:52 +0100

    get flow from code

--- calendarConnection.py
+++ calendarConnection.py
@@ -8,6 +8,7 @@ import sys
 from apiclient import discovery
 from oauth2client import client
 from oauth2client import tools
+from oauth2client import clientsecrets
 from oauth2client.file import Storage
 import googleapiclient
 
@@ -44,7 +45,7 @@ def get_credentials():
     store = Storage(credential_path)
     credentials = store.get()
     if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow = flow_from_json(CLIENT_SECRET, SCOPES)
         flow.user_agent = APPLICATION_NAME
 
         credentials = tools.run_flow(flow, store, FuckYourFlags())
@@ -102,3 +103,75 @@ def insertEvent(year, month, day, name, calendar="Dienstplan"):
     event = service.events().insert(calendarId=calendar_id, body=event).execute()
 
     return event
+
+
+def flow_from_json(json, scope, redirect_uri=None,
+                    message=None, cache=None, login_hint=None,
+                    device_uri=None, pkce=None, code_verifier=None):
+    """Create a Flow from a clientsecrets json (mostly copied from oauth2client.client)
+
+    Will create the right kind of Flow based on the contents of the
+    clientsecrets json or will raise InvalidClientSecretsError for unknown
+    types of Flows.
+
+    Args:
+        filename: string, the json containing the client secrets.
+        scope: string or iterable of strings, scope(s) to request.
+        redirect_uri: string, Either the string 'urn:ietf:wg:oauth:2.0:oob' for
+                      a non-web-based application, or a URI that handles the
+                      callback from the authorization server.
+        message: string, A friendly string to display to the user if the
+                 clientsecrets file is missing or invalid. If message is
+                 provided then sys.exit will be called in the case of an error.
+                 If message in not provided then
+                 clientsecrets.InvalidClientSecretsError will be raised.
+        cache: An optional cache service client that implements get() and set()
+               methods. See clientsecrets.loadfile() for details.
+        login_hint: string, Either an email address or domain. Passing this
+                    hint will either pre-fill the email box on the sign-in form
+                    or select the proper multi-login session, thereby
+                    simplifying the login flow.
+        device_uri: string, URI for device authorization endpoint. For
+                    convenience defaults to Google's endpoints but any
+                    OAuth 2.0 provider can be used.
+
+    Returns:
+        A Flow object.
+
+    Raises:
+        UnknownClientSecretsFlowError: if the file describes an unknown kind of
+                                       Flow.
+        clientsecrets.InvalidClientSecretsError: if the clientsecrets file is
+                                                 invalid.
+    """
+    try:
+        client_type, client_info = clientsecrets.loads(json)
+        if client_type in (clientsecrets.TYPE_WEB,
+                           clientsecrets.TYPE_INSTALLED):
+            constructor_kwargs = {
+                'redirect_uri': redirect_uri,
+                'auth_uri': client_info['auth_uri'],
+                'token_uri': client_info['token_uri'],
+                'login_hint': login_hint,
+            }
+            revoke_uri = client_info.get('revoke_uri')
+            optional = ('revoke_uri', 'device_uri', 'pkce', 'code_verifier')
+            for param in optional:
+                if locals()[param] is not None:
+                    constructor_kwargs[param] = locals()[param]
+
+            return client.OAuth2WebServerFlow(
+                client_info['client_id'], client_info['client_secret'],
+                scope, **constructor_kwargs)
+
+    except clientsecrets.InvalidClientSecretsError as e:
+        if message is not None:
+            if e.args:
+                message = ('The client secrets were invalid: '
+                           '\n{0}\n{1}'.format(e, message))
+            sys.exit(message)
+        else:
+            raise
+    else:
+        raise client.UnknownClientSecretsFlowError(
+            'This OAuth 2.0 flow is unsupported: {0!r}'.format(client_type))
